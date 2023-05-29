82b798cb347be4ad4f4e3bf9a765f3dffed37945
Aleksandar Galbulev <aleksandar.galbulev@iodigital.com>
2023-05-29 16:29:58 +0300

    More changes

--- nextjs/my-umbraco-project/src/components/pages/ContentPage.tsx
+++ nextjs/my-umbraco-project/src/components/pages/ContentPage.tsx
@@ -1,21 +1,6 @@
 import IUmbracoContent from "@/src/interfaces/IUmbracoContent";
 import Navigation from "../content/navigtion/Navigation";
-import IUmbracoRoute from "@/src/interfaces/IUmbracoRoute";
-
-const getCulture = (path: string, cultures: { [key: string]: IUmbracoRoute } ) => {
-    const keys = Object.keys(cultures);
-    let culture = 'en-us';
-
-    const currentCultures =  keys.filter(key => {
-        return cultures[key].path === path;
-    })
-
-    if (currentCultures.length !== 0) {
-        culture = currentCultures[0];
-    } 
-
-    return culture;
-}
+import getCulture from "@/src/helpers/getCulture";
 
 const ContentPage = ({ properties, contentType, route, cultures }: IUmbracoContent<any>) => {
     const culture = getCulture(route.path, cultures);
---
+++ nextjs/my-umbraco-project/src/helpers/getCulture.ts
@@ -0,0 +1,18 @@
+import IUmbracoRoute from "../interfaces/IUmbracoRoute";
+
+const getCulture = (path: string, cultures: { [key: string]: IUmbracoRoute } ) => {
+    const keys = Object.keys(cultures);
+    let culture = 'en-us';
+
+    const currentCultures =  keys.filter(key => {
+        return cultures[key].path === path;
+    })
+
+    if (currentCultures.length !== 0) {
+        culture = currentCultures[0];
+    } 
+
+    return culture;
+}
+
+export default getCulture;
